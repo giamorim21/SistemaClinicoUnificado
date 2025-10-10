9aa2b5c2ceb10f9b950fa7bd9bacf6b8c14ea4dc
Joao <motajoao41@sempreceub.com>
2025-10-10 11:59:41 -0300

    página de login e registro criadas

--- clinic-system/frontend/sistema-clinico-unificado-frontend/src/App.jsx
+++ clinic-system/frontend/sistema-clinico-unificado-frontend/src/App.jsx
@@ -1,33 +1,27 @@
-// import React from "react";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import "./styles/global.css";
-// import "./styles/theme.css";
-// import Home from "./pages/Home";
-// import QuemSomos from "./pages/QuemSomos";
-
-// export default function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/quem-somos" element={<QuemSomos />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
 import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import TelaLogin from "./pages/Login";
+import TelaCadastro from "./pages/Register";
 
-import "./styles/theme.css";
-import "./styles/global.css";
+import "./styles/home.css";
+import "./styles/components.css";
+import "./styles/register.css";
+import "./styles/login.css";
+
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<TelaLogin />} />
+        <Route path="/register" element={<TelaCadastro />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
       <Footer />
     </>
   );
--- clinic-system/frontend/sistema-clinico-unificado-frontend/src/main.jsx
+++ clinic-system/frontend/sistema-clinico-unificado-frontend/src/main.jsx
@@ -1,33 +1,13 @@
-// import React from 'react';
-// import { createRoot } from 'react-dom/client';
-// import App from './App.jsx';
-
-// // Bootstrap (via npm)
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-// import 'bootstrap-icons/font/bootstrap-icons.css';
-
-
-// // Seus estilos
-// import './styles/global.css';
-// import './styles/theme.css';
-
-// createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-
-// Se você estiver usando CDN do Bootstrap, mantenha as tags no index.html.
-// Se preferir via NPM, descomente a linha abaixo:
-// import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
